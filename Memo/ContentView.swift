46082257965ff4657ca386a63fc6ab8daec8c4d2
DopeKS <kyusang.dev@gmail.com>
2024-04-22 16:01:02 +0900

    Implement memo contentView

--- Memo/ContentView.swift
+++ Memo/ContentView.swift
@@ -47,14 +47,53 @@ class MemoStore: ObservableObject {
 }
 
 struct ContentView: View {
+    @ObservedObject var memoStore: MemoStore = MemoStore()
+    
+    @State var isSheetShowing: Bool = false
+    @State var memoText: String = ""
+    @State var memoColor: Color = .blue
+    
+    let colors: [Color] = [.blue, .cyan, .yellow, .indigo]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(memoStore.memos) { memo in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("\(memo.text)")
+                            .font(.title)
+                        Text("\(memo.createdString)")
+                            .font(.body)
+                            .padding(.top)
+                    }
+                    Spacer()
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(memo.color)
+                .shadow(radius: 3)
+                .padding()
+                .contextMenu {
+                    ShareLink(item: memo.text)
+                    Button {
+                        memoStore.removeMemo(memo)
+                    } label: {
+                        Image(systemName: "trash.slash")
+                        Text("삭제")
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("memo")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("추가") {
+                        memoText = ""
+                        isSheetShowing = true
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
